9a3580ca18ce540bfa16bfef798cd572076a0fc4
Beorlor <jbdurand2000@gmail.com>
2025-02-25 00:06:29 +0100

    ioat with print

--- iota/iota.go
+++ iota/iota.go
@@ -4,6 +4,21 @@ import "fmt"
 
 type Color int
 
+func (c Color) String() string {
+	switch c {
+	case ColorBlue:
+		return "BLUE"
+	case ColorBlack:
+		return "BLACK"
+	case ColorYellow:
+		return "YELLOW"
+	case ColorPink:
+		return "PINK"
+	default:
+			panic("Invalid color given")
+	}
+}
+
 const (
 	ColorBlue Color = iota
 	ColorBlack
