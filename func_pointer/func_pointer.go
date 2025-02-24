e1f80892e5b22a89c2a2b72610cfba444a880747
Beorlor <jbdurand2000@gmail.com>
2025-02-25 00:30:04 +0100

    func pointer

---
+++ func_pointer/func_pointer.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type TransformFunc func (s string) string
+
+func Uppercase(s string) string {
+	return strings.ToUpper(s)
+}
+
+func transformString(s string, fn TransformFunc) string {
+	return fn(s)
+}
+
+func main() {
+	fmt.Println(transformString("hello Sailor!", Uppercase))
+}
