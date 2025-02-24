b48b0d1cfa5c3e2251ddd6a15ff79c7ad011474b
Beorlor <jbdurand2000@gmail.com>
2025-02-25 00:32:58 +0100

    more complex func pointer pattern

--- func_pointer/func_pointer.go
+++ func_pointer/func_pointer.go
@@ -11,10 +11,17 @@ func Uppercase(s string) string {
 	return strings.ToUpper(s)
 }
 
+func Prefixer(prefix string) TransformFunc {
+	return func(s string) string {
+		return prefix + s
+	}
+}
+
 func transformString(s string, fn TransformFunc) string {
 	return fn(s)
 }
 
 func main() {
 	fmt.Println(transformString("hello Sailor!", Uppercase))
+	fmt.Println(transformString("hello Sailor!", Prefixer("FOO")))
 }
